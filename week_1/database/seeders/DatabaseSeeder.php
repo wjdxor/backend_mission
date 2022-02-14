2fd567e76df4197c59ee5c5d7c61bd87e2b8cef7
LIM JT(임정택) <38105420+wjdxor@users.noreply.github.com>
2022-02-14 19:29:00 +0900

    라라벨 블레이드 프로젝트 생성

---
+++ week_1/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+// database/seeders/DatabaseSeeder.php 
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            AdminSeeder::class,
+            UserSeeder::class
+        ]);
+    }
+}
